445c00952379ea5d948803bd31da40c7cb7a3b95
Stephen LaPorte <stephen.laporte@gmail.com>
2013-12-22 03:46:51 +0000

    disambig-related operations

--- wapiti/operations/query_operations.py
+++ wapiti/operations/query_operations.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from collections import namedtuple
 
 from base import QueryOperation
 from params import SingleParam, StaticParam
@@ -7,9 +8,23 @@ from params import SingleParam, StaticParam
 from models import PageInfo
 
 
+DabLink = namedtuple('DabLink', 'title ns dab_link dab_link_ns')
+
 class GetQueryPage(QueryOperation):
     field_prefix = 'gqp'
     input_field = SingleParam('page')
+    """
+    Valid pages:
+
+    Ancientpages, BrokenRedirects, Deadendpages, Disambiguations,
+    DoubleRedirects, Listredirects, Lonelypages, Longpages, Mostcategories,
+    Mostimages, Mostinterwikis, Mostlinkedcategories, Mostlinkedtemplates,
+    Mostlinked, Mostrevisions, Fewestrevisions, Shortpages,
+    Uncategorizedcategories, Uncategorizedpages, Uncategorizedimages,
+    Uncategorizedtemplates, Unusedcategories, Unusedimages, Wantedcategories,
+    Wantedfiles, Wantedpages, Wantedtemplates, Unwatchedpages, Unusedtemplates,
+    Withoutinterwiki, DisambiguationPages, DisambiguationPageLinks
+    """
     fields = [StaticParam('generator', 'querypage'),
               StaticParam('prop', 'info'),
               StaticParam('inprop', 'subjectid|talkid|protection')]
@@ -26,7 +41,7 @@ class GetQueryPage(QueryOperation):
     def prepare_params(self, **kw):
         params = super(GetQueryPage, self).prepare_params(**kw)
         if params.get('gqpcontinue'):
-            params['gqpoffset'] = params.pop('ggqpcontinue')
+            params['gqpoffset'] = params.pop('gqpcontinue')
         return params
 
 
@@ -34,6 +49,22 @@ class GetAncientPages(GetQueryPage):
     input_field = None
     fields = GetQueryPage.fields + [StaticParam('gqppage', 'Ancientpages')]
 
+
+class GetDabPageLinks(GetQueryPage):
+    field_prefix = 'qp'
+    input_field = None
+    fields = [StaticParam('list', 'querypage'),
+              StaticParam('qppage', 'DisambiguationPageLinks')]
+    output_type = [DabLink]
+
+    def extract_results(self, qp_res):
+        ret = []
+        for page in qp_res['querypage']['results']:
+            ret.append(DabLink(page['title'],
+                               page['ns'],
+                               page['databaseResult']['to_namespace'],
+                               page['databaseResult']['to_title']))
+        return ret
 
 class GetBrokenRedirects(GetQueryPage):
     input_field = None
